85bc1785c172d39de8c357442ccc1eff946ff56c
Yulia <yulia.kotseruba@gmail.com>
2020-01-02 15:04:05 -0500

    updated download_clips.sh
    added option to skip downloaded videos

--- download_clips.sh
+++ download_clips.sh
@@ -1,3 +1,3 @@
 # Download PIE clips
 
-wget --recursive --no-parent -nH --cut-dirs=1 -R "index.html*" https://data.nvision2.eecs.yorku.ca/PIE_dataset/PIE_clips/ .
+wget -N --recursive --no-parent -nH --cut-dirs=1 -R "index.html*" https://data.nvision2.eecs.yorku.ca/PIE_dataset/PIE_clips/ .
